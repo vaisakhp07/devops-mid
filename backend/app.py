9ca416d120cf2362ef5a8f4e7ebb8afdf6fcaa8b
demo <demo@gmail.com>
2026-04-10 23:03:45 +0530

    nth fix

--- backend/app.py
+++ backend/app.py
@@ -1,39 +1,18 @@
-from flask import Flask, jsonify
-import time
-import random
+from flask import Flask
+from prometheus_client import Counter, Histogram, generate_latest
 
 app = Flask(__name__)
 
-start_time = time.time()
+# Metrics
+REQUEST_COUNT = Counter('app_requests_total', 'Total Requests')
+REQUEST_LATENCY = Histogram('app_request_latency_seconds', 'Request latency')
 
 @app.route("/")
 def home():
-    return "DevOps Project Backend Running......fdghjghgfd"
-
-@app.route("/health")
-def health():
-    return jsonify({"status": "healthy"})
-
-@app.route("/users")
-def users():
-    sample_users = [
-        {"id": 1, "name": "Alice"},
-        {"id": 2, "name": "Bob"},
-    ]
-    return jsonify(sample_users)
+    REQUEST_COUNT.inc()
+    with REQUEST_LATENCY.time():
+        return "Hello DevOps 🚀"
 
 @app.route("/metrics")
 def metrics():
-    uptime = time.time() - start_time
-    return f"""
-# HELP app_uptime_seconds Uptime of the app
-# TYPE app_uptime_seconds counter
-app_uptime_seconds {uptime}
-
-# HELP random_value Random metric
-# TYPE random_value gauge
-random_value {random.randint(1,100)}
-"""
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return generate_latest(), 200, {'Content-Type': 'text/plain'}
